dfb7dac9f016c78ddebca0d2817c40644cfe5383
mudkipme <i@mudkip.me>
2013-09-21 01:08:50 +0800

    Johto and Hoenn map data.

--- public/javascripts/data/map.js
+++ public/javascripts/data/map.js
@@ -316,10 +316,301 @@ define([], function(){
       width: 557
       ,height: 374
       ,towns: [
+        {
+          name: 'new-bark-town'
+          ,x: 448
+          ,y: 268
+        }
+        ,{
+          name: 'cherrygrove-city'
+          ,x: 365
+          ,y: 268
+        }
+        ,{
+          name: 'violet-city'
+          ,x: 288
+          ,y: 176
+        }
+        ,{
+          name: 'azalea-town'
+          ,x: 273
+          ,y: 326
+        }
+        ,{
+          name: 'goldenrod-city'
+          ,x: 242
+          ,y: 238
+        }
+        ,{
+          name: 'ecruteak-city'
+          ,x: 258
+          ,y: 122
+        }
+        ,{
+          name: 'olivine-city'
+          ,x: 175
+          ,y: 175
+        }
+        ,{
+          name: 'cianwood-city'
+          ,x: 111
+          ,y: 245
+        }
+        ,{
+          name: 'mahogany-town'
+          ,x: 348
+          ,y: 122
+        }
+        ,{
+          name: 'blackthorn-city'
+          ,x: 429
+          ,y: 122
+        }
       ]
       ,spots: [
+        {
+          name: 'dark-cave'
+          ,x: 416
+          ,y: 226
+        }
+        ,{
+          name: 'sprout-tower'
+          ,x: 321
+          ,y: 157
+        }
+        ,{
+          name: 'ruins-of-alph'
+          ,x: 288
+          ,y: 210
+        }
+        ,{
+          name: 'union-cave'
+          ,x: 288
+          ,y: 288
+        }
+        ,{
+          name: 'slowpoke-well'
+          ,x: 272
+          ,y: 302
+        }
+        ,{
+          name: 'ilex-forest'
+          ,x: 242
+          ,y: 326
+        }
+        ,{
+          name: 'natural-park'
+          ,x: 232
+          ,y: 162
+        }
+        ,{
+          name: 'burned-tower'
+          ,x: 258
+          ,y: 97
+        }
+        ,{
+          name: 'cliff-cave'
+          ,x: 58
+          ,y: 263
+        }
+        ,{
+          name: 'whirl-islands'
+          ,x: 140
+          ,y: 227
+        }
+        ,{
+          name: 'mt-mortar'
+          ,x: 309
+          ,y: 97
+        }
+        ,{
+          name: 'lake-of-rage'
+          ,x: 348
+          ,y: 44
+        }
+        ,{
+          name: 'ice-path'
+          ,x: 460
+          ,y: 100
+        }
+        ,{
+          name: 'dragon-den'
+          ,x: 440
+          ,y: 92
+        }
+        ,{
+          name: 'mt-silver'
+          ,x: 504
+          ,y: 183
+        }
+        ,{
+          name: 'tohjo-falls'
+          ,x: 490
+          ,y: 255
+        }
       ]
       ,routes: [
+        {
+          name: 'johto-route-46'
+          ,direction: 'vertical'
+          ,x: 416
+          ,y1: 226
+          ,y2: 268
+        }
+        ,{
+          name: 'johto-route-29'
+          ,direction: 'horizontal'
+          ,y: 268
+          ,x1: 448
+          ,x2: 365
+        }
+        ,{
+          name: 'johto-route-31'
+          ,direction: 'horizontal'
+          ,y: 176
+          ,x1: 365
+          ,x2: 288
+        }
+        ,{
+          name: 'johto-route-30'
+          ,direction: 'vertical'
+          ,x: 365
+          ,y1: 268
+          ,y2: 176
+        }
+        ,{
+          name: 'johto-route-32'
+          ,direction: 'vertical'
+          ,x: 288
+          ,y1: 176
+          ,y2: 288
+        }
+        ,{
+          name: 'johto-route-33'
+          ,direction: 'vertical'
+          ,x: 288
+          ,y1: 288
+          ,y2: 326
+        }
+        ,{
+          name: 'johto-route-33'
+          ,direction: 'horizontal'
+          ,y: 326
+          ,x1: 288
+          ,x2: 242
+        }
+        ,{
+          name: 'johto-route-34'
+          ,direction: 'vertical'
+          ,x: 242
+          ,y1: 326
+          ,y2: 238
+        }
+        ,{
+          name: 'johto-route-35'
+          ,direction: 'vertical'
+          ,x: 242
+          ,y1: 238
+          ,y2: 176
+        }
+        ,{
+          name: 'johto-route-37'
+          ,direction: 'vertical'
+          ,x: 258
+          ,y1: 176
+          ,y2: 122
+        }
+        ,{
+          name: 'johto-route-36'
+          ,direction: 'horizontal'
+          ,y: 176
+          ,x1: 242
+          ,x2: 288
+        }
+        ,{
+          name: 'johto-route-38'
+          ,direction: 'horizontal'
+          ,y: 122
+          ,x1: 258
+          ,x2: 175
+        }
+        ,{
+          name: 'johto-route-39'
+          ,direction: 'vertical'
+          ,x: 175
+          ,y1: 122
+          ,y2: 175
+        }
+        ,{
+          name: 'johto-route-40'
+          ,direction: 'horizontal'
+          ,y: 175
+          ,x1: 175
+          ,x2: 154
+        }
+        ,{
+          name: 'johto-route-40'
+          ,direction: 'vertical'
+          ,x: 154
+          ,y1: 175
+          ,y2: 245
+        }
+        ,{
+          name: 'johto-route-41'
+          ,direction: 'horizontal'
+          ,y: 245
+          ,x1: 154
+          ,x2: 111
+        }
+        ,{
+          name: 'johto-route-42'
+          ,direction: 'horizontal'
+          ,y: 122
+          ,x1: 258
+          ,x2: 348
+        }
+        ,{
+          name: 'johto-route-43'
+          ,direction: 'vertical'
+          ,x: 348
+          ,y1: 122
+          ,y2: 44
+        }
+        ,{
+          name: 'johto-route-44'
+          ,direction: 'horizontal'
+          ,y: 122
+          ,x1: 348
+          ,x2: 429
+        }
+        ,{
+          name: 'johto-route-45'
+          ,direction: 'vertical'
+          ,x: 429
+          ,y1: 122
+          ,y2: 226
+        }
+        ,{
+          name: 'johto-route-45'
+          ,direction: 'horizontal'
+          ,y: 226
+          ,x1: 429
+          ,x2: 416
+        }
+        ,{
+          name: 'johto-route-48'
+          ,direction: 'vertical'
+          ,x: 46
+          ,y1: 245
+          ,y2: 182
+        }
+        ,{
+          name: 'johto-route-47'
+          ,direction: 'horizontal'
+          ,y: 245
+          ,x1: 111
+          ,x2: 46
+        }
       ]
     }
 
@@ -327,10 +618,458 @@ define([], function(){
       width: 566
       ,height: 336
       ,towns: [
+        {
+          name: 'littleroot-town'
+          ,x: 127
+          ,y: 247
+        }
+        ,{
+          name: 'oldale-town'
+          ,x: 127
+          ,y: 206
+        }
+        ,{
+          name: 'petalburg-city'
+          ,x: 80
+          ,y: 206
+        }
+        ,{
+          name: 'rustboro-city'
+          ,x: 56
+          ,y: 135
+        }
+        ,{
+          name: 'dewford-town'
+          ,x: 103
+          ,y: 287
+        }
+        ,{
+          name: 'slateport-city'
+          ,x: 191
+          ,y: 227
+        }
+        ,{
+          name: 'mauville-city'
+          ,x: 191
+          ,y: 154
+        }
+        ,{
+          name: 'verdanturf-town'
+          ,x: 127
+          ,y: 154
+        }
+        ,{
+          name: 'fallarbor-town'
+          ,x: 107
+          ,y: 62
+        }
+        ,{
+          name: 'lavaridge-town'
+          ,x: 148
+          ,y: 111
+        }
+        ,{
+          name: 'fortree-city'
+          ,x: 238
+          ,y: 61
+        }
+        ,{
+          name: 'lilycove-city'
+          ,x: 363
+          ,y: 106
+        }
+        ,{
+          name: 'mossdeep-city'
+          ,x: 463
+          ,y: 135
+        }
+        ,{
+          name: 'sootopolis-city'
+          ,x: 407
+          ,y: 177
+        }
+        ,{
+          name: 'pacifidlog-town'
+          ,x: 335
+          ,y: 227
+        }
+        ,{
+          name: 'ever-grande-city'
+          ,x: 504
+          ,y: 196
+        }
       ]
       ,spots: [
+        {
+          name: 'petalburg-woods'
+          ,x: 56
+          ,y: 183
+        }
+        ,{
+          name: 'rusturf-tunnel'
+          ,x: 127
+          ,y: 135
+        }
+        ,{
+          name: 'granite-cave'
+          ,x: 73
+          ,y: 287
+        }
+        ,{
+          name: 'abandoned-ship'
+          ,x: 191
+          ,y: 287
+        }
+        ,{
+          name: 'new-mauville'
+          ,x: 191
+          ,y: 187
+        }
+        ,{
+          name: 'fiery-path'
+          ,x: 168
+          ,y: 111
+        }
+        ,{
+          name: 'jagged-path'
+          ,x: 204
+          ,y: 111
+        }
+        ,{
+          name: 'meteor-falls'
+          ,x: 70
+          ,y: 100
+        }
+        ,{
+          name: 'hoenn-safari-zone'
+          ,x: 294
+          ,y: 74
+        }
+        ,{
+          name: 'mt-pyre'
+          ,x: 325
+          ,y: 131
+        }
+        ,{
+          name: 'shoal-cave'
+          ,x: 463
+          ,y: 105
+        }
+        ,{
+          name: 'sky-pillar'
+          ,x: 389
+          ,y: 213
+        }
+        ,{
+          name: 'artisan-cave'
+          ,x: 419
+          ,y: 257
+        }
       ]
       ,routes: [
+        {
+          name: 'hoenn-route-101'
+          ,direction: 'vertical'
+          ,x: 127
+          ,y1: 247
+          ,y2: 206
+        }
+        ,{
+          name: 'hoenn-route-102'
+          ,direction: 'horizontal'
+          ,y: 206
+          ,x1: 127
+          ,x2: 80
+        }
+        ,{
+          name: 'hoenn-route-103'
+          ,direction: 'vertical'
+          ,x: 127
+          ,y1: 206
+          ,y2: 187
+        }
+        ,{
+          name: 'hoenn-route-103'
+          ,direction: 'horizontal'
+          ,y: 187
+          ,x1: 127
+          ,x2: 191
+        }
+        ,{
+          name: 'hoenn-route-104'
+          ,direction: 'vertical'
+          ,x: 56
+          ,y1: 206
+          ,y2: 135
+        }
+        ,{
+          name: 'hoenn-route-104'
+          ,direction: 'horizontal'
+          ,y: 206
+          ,x1: 80
+          ,x2: 56
+        }
+        ,{
+          name: 'hoenn-route-105'
+          ,direction: 'vertical'
+          ,x: 56
+          ,y1: 206
+          ,y2: 270
+        }
+        ,{
+          name: 'hoenn-route-106'
+          ,direction: 'horizontal'
+          ,y: 270
+          ,x1: 56
+          ,x2: 103
+        }
+        ,{
+          name: 'hoenn-route-106'
+          ,direction: 'vertical'
+          ,x: 103
+          ,y1: 270
+          ,y2: 287
+        }
+        ,{
+          name: 'hoenn-route-107'
+          ,direction: 'horizontal'
+          ,y: 287
+          ,x1: 103
+          ,x2: 147
+        }
+        ,{
+          name: 'hoenn-route-108'
+          ,direction: 'horizontal'
+          ,y: 287
+          ,x1: 147
+          ,x2: 191
+        }
+        ,{
+          name: 'hoenn-route-109'
+          ,direction: 'vertical'
+          ,x: 191
+          ,y1: 287
+          ,y2: 227
+        }
+        ,{
+          name: 'hoenn-route-110'
+          ,direction: 'vertical'
+          ,x: 191
+          ,y1: 227
+          ,y2: 154
+        }
+        ,{
+          name: 'hoenn-route-112'
+          ,direction: 'horizontal'
+          ,y: 111
+          ,x1: 191
+          ,x2: 148
+        }
+        ,{
+          name: 'hoenn-route-113'
+          ,direction: 'horizontal'
+          ,y: 62
+          ,x1: 191
+          ,x2: 107
+        }
+        ,{
+          name: 'hoenn-route-111'
+          ,direction: 'vertical'
+          ,x: 191
+          ,y1: 154
+          ,y2: 62
+        }
+        ,{
+          name: 'hoenn-route-114'
+          ,direction: 'diagonal'
+          ,y1: 62
+          ,y2: 92
+          ,x1: 107
+          ,x2: 56
+        }
+        ,{
+          name: 'hoenn-route-115'
+          ,direction: 'vertical'
+          ,x: 56
+          ,y1: 92
+          ,y2: 135
+        }
+        ,{
+          name: 'hoenn-route-116'
+          ,direction: 'horizontal'
+          ,y: 135
+          ,x1: 56
+          ,x2: 127
+        }
+        ,{
+          name: 'hoenn-route-116'
+          ,direction: 'vertical'
+          ,x: 127
+          ,y1: 135
+          ,y2: 154
+        }
+        ,{
+          name: 'hoenn-route-117'
+          ,direction: 'horizontal'
+          ,y: 154
+          ,x1: 127
+          ,x2: 191
+        }
+        ,{
+          name: 'hoenn-route-118'
+          ,direction: 'horizontal'
+          ,y: 154
+          ,x1: 191
+          ,x2: 219
+        }
+        ,{
+          name: 'hoenn-route-119'
+          ,direction: 'vertical'
+          ,x: 219
+          ,y1: 154
+          ,y2: 61
+        }
+        ,{
+          name: 'hoenn-route-119'
+          ,direction: 'horizontal'
+          ,y: 61
+          ,x1: 219
+          ,x2: 238
+        }
+        ,{
+          name: 'hoenn-route-120'
+          ,direction: 'horizontal'
+          ,y: 61
+          ,x1: 238
+          ,x2: 261
+        }
+        ,{
+          name: 'hoenn-route-120'
+          ,direction: 'vertical'
+          ,x: 261
+          ,y1: 61
+          ,y2: 106
+        }
+        ,{
+          name: 'hoenn-route-121'
+          ,direction: 'horizontal'
+          ,y: 106
+          ,x1: 261
+          ,x2: 363
+        }
+        ,{
+          name: 'hoenn-route-122'
+          ,direction: 'vertical'
+          ,x: 325
+          ,y1: 106
+          ,y2: 154
+        }
+        ,{
+          name: 'hoenn-route-123'
+          ,direction: 'horizontal'
+          ,y: 154
+          ,x1: 219
+          ,x2: 325
+        }
+        ,{
+          name: 'hoenn-route-124'
+          ,direction: 'diagonal'
+          ,x1: 363
+          ,x2: 463
+          ,y1: 106
+          ,y2: 135
+        }
+        ,{
+          name: 'hoenn-route-125'
+          ,direction: 'vertical'
+          ,x: 463
+          ,y1: 135
+          ,y2: 105
+        }
+        ,{
+          name: 'hoenn-route-126'
+          ,direction: 'vertical'
+          ,x: 383
+          ,y1: 153
+          ,y2: 201
+        }
+        ,{
+          name: 'hoenn-route-126'
+          ,direction: 'horizontal'
+          ,y: 153
+          ,x1: 383
+          ,x2: 432
+        }
+        ,{
+          name: 'hoenn-route-126'
+          ,direction: 'vertical'
+          ,x: 432
+          ,y1: 153
+          ,y2: 201
+        }
+        ,{
+          name: 'hoenn-route-126'
+          ,direction: 'horizontal'
+          ,y: 201
+          ,x1: 383
+          ,x2: 432
+        }
+        ,{
+          name: 'hoenn-route-127'
+          ,direction: 'vertical'
+          ,x: 463
+          ,y1: 135
+          ,y2: 227
+        }
+        ,{
+          name: 'hoenn-route-128'
+          ,direction: 'horizontal'
+          ,y: 196
+          ,x1: 463
+          ,x2: 504
+        }
+        ,{
+          name: 'hoenn-route-131'
+          ,direction: 'horizontal'
+          ,y: 227
+          ,x1: 389
+          ,x2: 335
+        }
+        ,{
+          name: 'hoenn-route-130'
+          ,direction: 'horizontal'
+          ,y: 227
+          ,x1: 432
+          ,x2: 389
+        }
+        ,{
+          name: 'hoenn-route-129'
+          ,direction: 'horizontal'
+          ,y: 227
+          ,x1: 463
+          ,x2: 432
+        }
+        ,{
+          name: 'hoenn-route-134'
+          ,direction: 'horizontal'
+          ,y: 227
+          ,x1: 244
+          ,x2: 191
+        }
+        ,{
+          name: 'hoenn-route-133'
+          ,direction: 'horizontal'
+          ,y: 227
+          ,x1: 288
+          ,x2: 244
+        }
+        ,{
+          name: 'hoenn-route-132'
+          ,direction: 'horizontal'
+          ,y: 227
+          ,x1: 335
+          ,x2: 288
+        }
       ]
     }
 
