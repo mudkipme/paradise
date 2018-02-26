129bcbc3593ed37c6c8d1b6f0aeb98c1401b50b7
Mudkip <i@mudkip.me>
2018-02-26 22:10:51 +0800

    Move to mudkipme/pokeapi-v2-typescript

--- __tests__/species.test.ts
+++ __tests__/species.test.ts
@@ -1,8 +1,14 @@
+import nconf from "../src/lib/config";
+nconf.set("pokeapi:hostName", "pokeapi.mudkip.me");
 import Species from "../src/models/species";
+
+jest.setTimeout(20000);
 
 describe("rakuen.species", () => {
     test("i heard you liek mudkipz", async () => {
         const species = await Species.find(258);
         expect(species.pokemonSpecies.name).toEqual("mudkip");
+        expect(species.pokemonForme.name).toEqual("mudkip");
+        expect(species.pokemon.name).toEqual("mudkip");
     });
 });
--- src/models/species.ts
+++ src/models/species.ts
@@ -1,20 +1,29 @@
-import { PokemonSpecies } from "pokedex-promise-v2";
+import { Pokemon, PokemonForm, PokemonSpecies } from "pokedex-promise-v2";
 import pokedex from "../lib/pokedex";
 
 interface ISpeciesOptions {
     pokemonSpecies: PokemonSpecies;
+    pokemonForme: PokemonForm;
+    pokemon: Pokemon;
 }
 
 export default class Species {
     public static async find(speciesNumber: number, formIdentifier?: string | null) {
         const pokemonSpecies = await pokedex.getPokemonSpeciesByName(speciesNumber);
-        const species = new Species({ pokemonSpecies });
+        const pokemonFormeName = formIdentifier ? `${pokemonSpecies.name}-${formIdentifier}` : pokemonSpecies.name;
+        const pokemonForme = await pokedex.getPokemonFormByName(pokemonFormeName);
+        const pokemon = await pokedex.resource(pokemonForme.pokemon.url);
+        const species = new Species({ pokemonSpecies, pokemonForme, pokemon });
         return species;
     }
 
     public readonly pokemonSpecies: PokemonSpecies;
+    public readonly pokemonForme: PokemonForm;
+    public readonly pokemon: Pokemon;
 
     private constructor(options: ISpeciesOptions) {
         this.pokemonSpecies = options.pokemonSpecies;
+        this.pokemonForme = options.pokemonForme;
+        this.pokemon = options.pokemon;
     }
 }
